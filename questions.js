f83e791800f2d04220a8028e286925012d51440c
Gustavo Martauro <134699668+cassssssssssssssssssiiiiiuuuuuuu@users.noreply.github.com>
2025-06-04 11:02:17 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,40 @@
+export default[
+    {
+        question: "Qual o ano de lançamento do Opala ?",
+        answers:  [
+            {Option: "1968", correct: true},
+            {Option: "1957", correct: false},
+            {Option: "1973", correct: false},
+            {Option: "1972", correct: false},
+        ],
+    },
+    {
+         question: "Qual era o carro que disputava comercialmente com ele ?",
+        answers:  [
+            {Option: "Ford Galaxie", correct: true},
+            {Option: "Dodge Dart", correct: true},
+            {Option: "Ford Maverick", correct: false},
+            {Option: "Chevrolet Impala", correct: false},
+        ],
+    }, 
+    {
+         question: "Quais foram as versões lançadas ?",
+        answers:  [
+            {Option: "Opala 2.5 (4 cilindros), Opala 3.8 (6 cilindros), ", correct: true},
+            {Option: "Opala 4.1 (anos 80 e 90), Opala 250-S", correct: true},
+            {Option: "Opala 2.2 (4 cilindros), Opala 1.0 (3 cilindros)", correct: false},
+        ]
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+]
